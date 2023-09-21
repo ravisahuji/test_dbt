707df501bc1ad5ea7460aa587eba339f314661d2
Ravi Pratap Sahu <ravisahuji.snow02@gmail.com>
2023-09-21 14:23:38 +0000

    Add fct_orders.dql

---
+++ models/staging/stripe/stg_payment.sql
@@ -0,0 +1,12 @@
+with payments as (
+    select 
+        id as payment_id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created
+    from raw.stripe.payment
+)
+
+select * from payments
